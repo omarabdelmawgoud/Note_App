56cc22c98560ee08428d2faba3221676e518e1ab
Omar Abdelmawgoud <omarabdelmawgoud12@gmail.com>
2025-12-18 13:21:15 +0200

    scafold

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,8 @@ class NoteApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: Scaffold(),
+    );
   }
 }
