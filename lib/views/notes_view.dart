624e4624b047f8dd1812e6389135aeb031e1fd97
Omar Abdelmawgoud <omarabdelmawgoud12@gmail.com>
2025-12-22 20:31:20 +0200

    add button and finish note bottom sheet

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -8,24 +8,27 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: NotesViewBody(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showModalBottomSheet(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(16),
-            ),
-            backgroundColor: const Color.fromARGB(255, 56, 53, 53),
-            context: context,
-            builder: (context) {
-              return NoteBottomSheet();
-            },
-          );
-        },
-        shape: CircleBorder(),
-        backgroundColor: kcolor,
-        child: Icon(Icons.add),
+    return SafeArea(
+      top: false,
+      child: Scaffold(
+        body: NotesViewBody(),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            showModalBottomSheet(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16),
+              ),
+              backgroundColor: const Color.fromARGB(255, 56, 53, 53),
+              context: context,
+              builder: (context) {
+                return NoteBottomSheet();
+              },
+            );
+          },
+          shape: CircleBorder(),
+          backgroundColor: kcolor,
+          child: Icon(Icons.add),
+        ),
       ),
     );
   }
--- lib/views/widgets/note_bottom_sheet.dart
+++ lib/views/widgets/note_bottom_sheet.dart
@@ -9,15 +9,43 @@ class NoteBottomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 8),
-      child: Column(
-        children: [
-          SizedBox(height: 24),
-          Text("New Note", style: TextStyle(color: kcolor, fontSize: 24)),
-          SizedBox(height: 24),
-          CustomTextField(hint: "Title"),
-          SizedBox(height: 16),
-          CustomTextField(hint: "Content", maxlines: 5),
-        ],
+      child: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(
+            children: [
+              SizedBox(height: 24),
+              Text("New Note", style: TextStyle(color: kcolor, fontSize: 24)),
+              SizedBox(height: 24),
+              CustomTextField(hint: "Title"),
+              SizedBox(height: 16),
+              CustomTextField(hint: "Content", maxlines: 5),
+              SizedBox(height: 32),
+              CustomButton(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 55,
+      width: MediaQuery.of(context).size.width,
+      decoration: BoxDecoration(
+        color: kcolor,
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Center(
+        child: Text(
+          "Add",
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
       ),
     );
   }
