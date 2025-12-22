979745ee1c6c52443a904e47cf359338ad013c2a
Omar Abdelmawgoud <omarabdelmawgoud12@gmail.com>
2025-12-23 00:27:26 +0200

    create note model

---
+++ lib/models/note_model.dart
@@ -0,0 +1,6 @@
+class NoteModel {
+  final String title, subtitle, date;
+  final int color;
+
+  NoteModel({required this.title, required this.subtitle, required this.date, required this.color});
+}
